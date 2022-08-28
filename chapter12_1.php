7884840d3fc336e71fcf17fa94620a53fc96044c
yuko <yuko@example.com>
2022-08-28 07:52:02 +0000

    chapter12_1を書いた

---
+++ chapter12_1.php
@@ -0,0 +1,31 @@
+<?php
+class MyTest2
+{
+    //
+    private $price;
+    
+    private $name;
+    
+    //
+public function setPrice($v)
+{
+    $this->Price = $v;
+}
+//
+public function getPrice()
+{
+    return $this->price;
+}
+public function setName($J)
+{
+    $this->name = $j;
+}
+//
+public function getName()
+{
+    return $this->name;
+}
+    
+}
+
+
