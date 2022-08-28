13f4ffd93ca041ef09c75ca030ef270f2b64d2d0
yuko <yuko@example.com>
2022-08-28 07:50:48 +0000

    chapter12.phpを書いた

---
+++ chapter12.php
@@ -0,0 +1,16 @@
+<?php
+
+class Mytest
+{
+    //
+    public function add($i,$j)
+    {
+        return  $i +$j;
+    }
+    //
+    public function sub($i,$j)
+    {
+        return $i-$j;
+    }
+
+}
